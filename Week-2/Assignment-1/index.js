48d13d01e99e792336d477776ee9c63b815448c5
Tim Chang <b00501007@gmail.com>
2020-04-22 14:38:58 +0800

    W2A1 v1.0

---
+++ Week-2/Assignment-1/index.js
@@ -0,0 +1,29 @@
+function max(numbers) {
+  // your code here, for-loop method preferred
+  let max_number = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max_number) {
+      max_number = numbers[i];
+    }
+  }
+  return max_number;
+}
+
+function findPosition(numbers, target) {
+  // your code here, for-loop method preferred
+  let index = -1;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === target) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+}
+
+console.log(max([1, 2, 4, 5])); // should print 5
+console.log(max([5, 2, 7, 1, 6])); // should print 7
+console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
+console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
+console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); // should print 2 (the first position)
+console.log(findPosition([5, 2, 7, 1, 6], 8)); // should print -1
