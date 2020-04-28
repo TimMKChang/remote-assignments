ef3a4e666164da39eb787814282370f738dfdea2
Tim Chang <b00501007@gmail.com>
2020-04-28 15:54:31 +0800

    Calculate time for different length array

--- Week-2/Assignment-5/index.js
+++ Week-2/Assignment-5/index.js
@@ -26,16 +26,46 @@ const selectionSort = require('./selectionSort.js');
 const getRandomNumberArray = require('./getRandomNumberArray.js')
 
 // const unsort = [10, 8, 9, 2, 6, 5, 7, 3, 1, 4];
-const unsort = getRandomNumberArray(20);
-console.log('sort:');
-console.log(unsort);
+// const unsort = getRandomNumberArray(10000);
+// console.log('sort:');
+// console.log(unsort);
 
-console.time('selection sort');
-console.log(selectionSort(unsort));
-console.timeEnd("selection sort");
+// console.time('selection sort');
+// console.log(selectionSort(unsort));
+// console.timeEnd("selection sort");
 
-console.log('');
+// console.log('');
 
-console.time('quick sort');
-console.log(quickSort(unsort, 0, unsort.length - 1));
-console.timeEnd("quick sort");
+// console.time('quick sort');
+// console.log(quickSort(unsort, 0, unsort.length - 1));
+// console.timeEnd("quick sort");
+
+const count = 1000;
+let record = 0;
+console.time('average');
+for (let i = 0; i < count; i++) {
+
+  let unsort = getRandomNumberArray(Math.floor(Math.pow(1.2, 1) * 10000));
+  let startTime = new Date();
+  selectionSort(unsort);
+  const a = new Date() - startTime;
+
+  startTime = new Date();
+  quickSort(unsort, 0, unsort.length - 1);
+  const b = new Date() - startTime;
+
+  unsort = getRandomNumberArray(Math.floor(Math.pow(1.2, 2) * 10000));
+  startTime = new Date();
+  selectionSort(unsort);
+  const c = new Date() - startTime;
+
+  startTime = new Date();
+  quickSort(unsort, 0, unsort.length - 1);
+  const d = new Date() - startTime;
+
+
+  record += ((c / d) / (a / b));
+}
+
+console.log(record / count)
+console.timeEnd('average');
