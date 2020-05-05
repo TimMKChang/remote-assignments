d5084245194bd7d49fac172509ff459e2f497ec3
Tim Chang <b00501007@gmail.com>
2020-05-05 18:05:00 +0800

    Add User_without_unique_key model by migrations

---
+++ Week-4/Assignment-3/models/user_without_unique_key.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User_without_unique_key = sequelize.define('User_without_unique_key', {
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User_without_unique_key.associate = function(models) {
+    // associations can be defined here
+  };
+  return User_without_unique_key;
+};
