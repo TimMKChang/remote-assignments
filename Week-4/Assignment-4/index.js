46c1829c42c9d82445c17674fb1734d03fe4c77b
Tim Chang <b00501007@gmail.com>
2020-05-05 09:15:20 +0800

    Remove callback from async/await

--- Week-4/Assignment-4/index.js
+++ Week-4/Assignment-4/index.js
@@ -11,10 +11,10 @@ function delayedResultPromise(n1, n2, delayTime) {
 delayedResultPromise(4, 5, 3000).then(console.log) // 9 (4+5) will be shown in the console after 3 seconds
 
 // async / await
-async function main(cb) {
+async function main() {
   // your code here, you should call delayedResultPromise here and get the result using async / await.
-  const result = await cb();
+  const result = await delayedResultPromise(-5, 10, 2000);
   console.log(result);
 }
-main(() => { return delayedResultPromise(-5, 10, 2000) }); // result will be shown in the console after <delayTime> seconds
+main(); // result will be shown in the console after <delayTime> seconds
 
